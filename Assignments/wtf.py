4916dd228a659e5636f4acc69605b62804ec8aed
Jason Goncalves <jgoncalves@protonmail.com>
2020-05-14 23:50:15 -0400

    import random assignment

---
+++ Assignments/wtf.py
@@ -0,0 +1,8 @@
+import random
+
+food = ['chicken', 'beef', 'fish','broccoli', 'yams', 'salad']
+
+random_food = random.choice(food)
+
+print(f'TRY NOT TO BURN DOWN YOUR HOUSE WHEN MAKING... {random_food}')
+
