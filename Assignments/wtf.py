7b6ba5f6367b8c07567646e468599cf861e3e149
Jason Goncalves <jgoncalves@protonmail.com>
2020-05-17 23:57:10 -0400

    cleaned up files - remove assignments folder and converted it to Simple_Projects folder

--- Assignments/wtf.py
+++
@@ -1,8 +0,0 @@
-import random
-
-food = ['chicken', 'beef', 'fish','broccoli', 'yams', 'salad']
-
-random_food = random.choice(food)
-
-print(f'TRY NOT TO BURN DOWN YOUR HOUSE WHEN MAKING... {random_food}')
-
